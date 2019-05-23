e0b67022a3cd9af6af774dbf2119e53afc16c34a
DuHyeon <engus983@gmail.com>
2019-05-23 13:22:48 +0900

    FEAT: #2 Hello World with Styled Components
    Used styled-component and branch processing css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React, { Fragment } from "react";
-import "./App.css";
+import React from "react";
+import styled from "styled-components";
 
 const App = () => {
   return (
-    <Fragment>
-      <Button danger />
-      <Button />
-    </Fragment>
+    <Container>
+      <Button danger> Hello </Button>
+      <Button> Danger </Button>
+    </Container>
   );
 };
 
-const Button = ({ danger }) => {
-  return (
-    <button
-      className={danger ? "button button--danger" : "button button--success"}
-    >
-      Hello
-    </button>
-  );
-};
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`;
 
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${({ danger }) => (danger ? "#e74c3c" : "#2ecc71")};
+`;
 export default App;
