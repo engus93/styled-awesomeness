a0bad5059f2520b644bec3f6aca994c6923008c9
DuHyeon <engus983@gmail.com>
2019-05-23 14:08:50 +0900

    FEAT: #4 Animations
    Make animation css with keyframes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
 const App = () => {
   const GlobalStyle = createGlobalStyle`
-  body{
-    padding: 0;
-    margin: 0;
-  }
-`;
+    body {
+      padding: 0;
+      margin: 0;
+    }
+  `;
 
   const Container = styled.div`
     height: 100vh;
@@ -28,19 +28,39 @@ const App = () => {
       outline: none;
     }
 
-    /* props branch processing css */
+    /* props branch processing CSS */
     background-color: ${({ danger }) => (danger ? "#e74c3c" : "#2ecc71")};
+    ${({ danger, rotationTime }) => {
+      if (danger) {
+        return css`
+          animation: ${rotation} ${rotationTime}s linear infinite;
+        `;
+      }
+    }}
   `;
 
   const Anchor = styled(Button.withComponent("a"))`
     text-decoration: none;
   `;
 
+  // Animation CSS
+  const rotation = keyframes`
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  `;
+
   return (
     <Container>
       <GlobalStyle />
-      <Button danger> Hello </Button>
-      <Button> Danger </Button>
+      <Button> Hello </Button>
+      <Button danger rotationTime={5}>
+        {" "}
+        Danger{" "}
+      </Button>
       <Anchor href="www.google.com">Go to google</Anchor>
     </Container>
   );
