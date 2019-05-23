19b2cc2c0b001ac14e86748c58327ed9c35e8bfe
DuHyeon <engus983@gmail.com>
2019-05-23 13:49:04 +0900

    FEAT: #3 injectGlobal and Extend
    Extend css and element with styled-component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 const App = () => {
+  const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+  const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: #bdc3c7;
+  `;
+
+  const Button = styled.button`
+    border-radius: 50px;
+    padding: 5px;
+    min-width: 120px;
+    color: white;
+    font-weight: 600;
+    -webkit-appearance: none;
+    cursor: pointer;
+    &:active,
+    &:focus {
+      outline: none;
+    }
+
+    /* props branch processing css */
+    background-color: ${({ danger }) => (danger ? "#e74c3c" : "#2ecc71")};
+  `;
+
+  const Anchor = styled(Button.withComponent("a"))`
+    text-decoration: none;
+  `;
+
   return (
     <Container>
+      <GlobalStyle />
       <Button danger> Hello </Button>
       <Button> Danger </Button>
+      <Anchor href="www.google.com">Go to google</Anchor>
     </Container>
   );
 };
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: #bdc3c7;
-`;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${({ danger }) => (danger ? "#e74c3c" : "#2ecc71")};
-`;
 export default App;
